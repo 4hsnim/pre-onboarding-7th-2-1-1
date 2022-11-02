b712e6c5e235eb848de10d548c67bc0c0eb48df8
4hsnim <4hsnim@naver.com>
2022-11-02 19:18:03 +0900

    feat: 글로벌 스타일 생성

---
+++ src/ui/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * {
+        box-sizing: border-box;
+        font-family: 'Inter',sans-serif;
+
+    }
+    a {
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyle;
